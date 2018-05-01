12d7d75b529eaa14736d9efd40bcbb107b79cd88
Shikhar <shikharcool07@gmail.com>
2018-05-01 13:55:51 +0530

    intro animation

--- app/src/main/java/com/example/subhamdivakar/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/subhamdivakar/passwordmanager/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.subhamdivakar.passwordmanager;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("sdsadsadasdsktsktsktskt");
-        System.out.print("dsafsrgsdths");
-        System.out.print("amit");
+
     }
 }
