d702c1f31cdbd2ba3b748ec41df242196e40a7d5
AMIT KARMAKAR <amitkkr09@gmail.com>
2018-05-01 16:02:44 +0530

    new line added

--- app/src/main/java/com/example/subhamdivakar/passwordmanager/MainActivity.java
+++ app/src/main/java/com/example/subhamdivakar/passwordmanager/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        int id=10;
     }
 }
