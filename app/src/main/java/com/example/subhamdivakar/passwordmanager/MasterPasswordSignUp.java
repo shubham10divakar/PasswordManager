6575ba3eb2d2fa7eb7cfb2ecc22a8b3c220d80f4
AMIT KARMAKAR <amitkkr09@gmail.com>
2018-05-03 23:40:12 +0530

    made LOGIN ACtivity
    doing work on New User activity

--- app/src/main/java/com/example/subhamdivakar/passwordmanager/MasterPasswordSignUp.java
+++ app/src/main/java/com/example/subhamdivakar/passwordmanager/MasterPasswordSignUp.java
@@ -2,12 +2,16 @@ package com.example.subhamdivakar.passwordmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MasterPasswordSignUp extends AppCompatActivity {
 
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_master_password_sign_up);
+
+        t1=(TextView)findViewById(R.id.textViewPassWrap);
     }
 }
